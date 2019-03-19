766c437aeec22fde335e01b5e3baec7f5d486378
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-03-18 22:38:21 -0400

    Chapter 2

--- src/Ch2.purs
+++ src/Ch2.purs
@@ -1,13 +1,18 @@
-module Main where
+module Ch2 where 
 
 import Prelude
 import Effect (Effect)
 import Effect.Console (logShow)
-import Math (sqrt)
+import Math (sqrt, pi)
 
 main :: Effect Unit
-main =
+main = do
   logShow $ diagonal 3.0 4.0
+  logShow $ circleArea 5.0
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt $ w * w + h * h
+
+circleArea :: Number -> Number
+circleArea r =
+    r * r * pi
